4a562aea348aae13e569761a7810b8d658889764
Tim Q <timmm.qin@gmail.com>
2026-01-09 11:02:18 +0800

    adsfad

--- CalculusPlayground/HomeView.swift
+++ CalculusPlayground/HomeView.swift
@@ -4,4 +4,35 @@
 //
 //  Created by Timothy Qin on 8/1/26.
 //
+import SwiftUI
+let views: [any IdentifiableView] = [TaylorSeriesPlayground(), TangentLinePlayground(), DerivativePlayground()]
 
+struct HomeView: View {
+    var body: some View {
+        NavigationStack {
+            Form {
+                ForEach(views) {view in
+                    NavigationLink("Taylor Series 2") {
+                        AnyView(view)
+                    }
+                    
+                    
+                }
+            }
+            .navigationTitle("Function Playground")
+        }
+    }
+}
+
+
+#Preview {
+    HomeView()
+}
+
+
+
+protocol IdentifiableView: Identifiable, View {
+    var id: UUID { get set }
+    var body: any View { get set }
+}
+    
